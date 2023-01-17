a470f605779c174a364ae000e31aef5aaf0c6fa1
Robert Figura <rsyanihhfigura03@gmail.com>
2023-01-17 15:56:07 +0800

    first commit

---
+++ src/Dashboard/Header/Header.jsx
@@ -0,0 +1,12 @@
+import { PureComponent } from "react";
+import "./Header.css";
+
+export default class Header extends PureComponent {
+  render() {
+    return (
+      <header className="header">
+        Robert Emoji Search
+      </header>
+    );
+  }
+}
